3111aacb449c3970f7cee4aa6a0053c3cbdcd666
Carla Souza <csouza@reliantsecurity.com>
2013-05-22 04:03:17 -0300

    Fully functional

--- bot.rb
+++ bot.rb
@@ -1,51 +1,108 @@
 require 'nokogiri'
 require 'open-uri'
+require 'cinch'
+require "cinch/plugins/identify"
 
-def init
-  @data = load_data
-end
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "localhost"
+    c.nick = 'fantoche'
+    c.realname = 'puppet doc bot'
+    c.channels = ["#t"]
+    c.user = 'carlasouza'
+    c.plugins.plugins = [Cinch::Plugins::Identify] # optionally add more plugins
+    c.plugins.options[Cinch::Plugins::Identify] = {
+      :username => "fantoche",
+      :password => "XXX",
+      :type     => :nickserv,
+    }
+  end
 
-def load_data
-  doc = Nokogiri::HTML(open('http://docs.puppetlabs.com/references/latest/type.html'))
-  a = doc.css('div.primary-content')
+  on :message, /^!reload/ do |m|
+    @data = load_data
+    m.reply 'Done'
+  end
 
-  data = {}
-  last_type = ''
-  last_subtype = ''
-  last_element = ''
-  a[0].children.entries.delete_if {|e| !(e.name=='h3' || e.name=='h4' || e.name='dl')}[12..-1].each { |c|
-    if c.name == 'h3'
-      last_element = 'h3'
-      last_type = c.children.first.text
-      data[last_type] ||= {}
-    elsif c.name == 'h4'
-      last_element = 'h4'
-      last_subtype = c.children.first.text
-      data[last_type][last_subtype] ||= {}
-    elsif c.name == 'dl' and last_element == 'h3'
-      data[last_type]['desc'] = c.children.text.gsub('\n',' ')
-      last_element = 'dl'
-    elsif c.name == 'dl' and last_element == 'h4'
-      last, param = '', ''
-      c.children.each {|dt|
-        if dt.name == 'dt'
-          param=dt.children.first.text
-          last='dt'
-        elsif last=='dt' and dt.name == 'dd'
-          data[last_type][last_subtype][param] = dt.children.first.text
-          last=''
+  on :message, /^!types/ do |m|
+    m.reply types
+  end
+
+  on :message, /^!type (.+)/ do |m, type|
+    m.reply("#{m.user.nick}: #{type}: #{desc_type(type) || 'invalid type'}")
+  end
+
+  on :message, /^!link (.+)/ do |m, type|
+    m.reply("#{m.user.nick}: #{type}: http://docs.puppetlabs.com/references/latest/type.html##{type}")
+  end
+
+  on :message, /^!parameters (.+)/ do |m, type|
+    m.reply("#{m.user.nick}: #{type}: #{parameters(type) || 'invalid type'}")
+  end
+
+  on :message, /^!parameter (.+?) (.+)/ do |m, type, param|
+    m.reply("#{m.user.nick}: #{type} - #{param}: #{desc_param(type,param) || 'invalid parameter' rescue 'invalid type'}")
+  end
+
+  helpers do
+    def load_data
+      doc = Nokogiri::HTML(open('http://docs.puppetlabs.com/references/latest/type.html'))
+      a = doc.css('div.primary-content')
+
+      data = {}
+      last_type = ''
+      last_subtype = ''
+      last_element = ''
+      a[0].children.entries.delete_if {|e| !(e.name=='h3' || e.name=='h4' || e.name='dl')}[12..-1].each { |c|
+        if c.name == 'h3'
+          last_element = 'h3'
+          last_type = c.children.first.text
+          data[last_type] ||= {}
+        elsif c.name == 'h4'
+          last_element = 'h4'
+          last_subtype = c.children.first.text
+          data[last_type][last_subtype] ||= {}
+        elsif c.name == 'dl' and last_element == 'h3'
+          data[last_type]['desc'] = c.children.text.gsub("\n",' ')
+          last_element = 'dl'
+        elsif c.name == 'dl' and last_element == 'h4'
+          last, param = '', ''
+          c.children.each {|dt|
+            if dt.name == 'dt'
+              param=dt.children.first.text
+              last='dt'
+            elsif last=='dt' and dt.name == 'dd'
+              data[last_type][last_subtype][param] = dt.children.first.text.gsub("\n",' ')
+              last=''
+            end
+          }
+          last_element = 'dl'
         end
       }
-      last_element = 'dl'
+      data
     end
-  }
-  data
+
+    def types
+      @data ||= load_data
+      @data.keys.join ', '
+    end
+
+    def desc_type type
+      @data ||= load_data
+      @data[type]['desc']
+    end
+
+    def parameters type
+      @data ||= load_data
+      @data[type]['Parameters'].keys.join ', '
+    end
+
+    def desc_param type, param
+      @data ||= load_data
+      @data[type]['Parameters'][param]
+    end
+
+  end
+
 end
 
-def types
-  @data.keys
-end
-
-def parameters type
-  @data[type]['Parameters'].keys
-end
+bot.start
